fd32acff935a683e7692e3e228cad3a0e0d07e5c
Adam Urbina <adamurbina@gmail.com>
2018-05-31 19:17:23 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
     newArray = Array.new
     index = 0
     while index < array.size
-        newArray = yield(array[index])
+        newArray << yield(array[index])
         index += 1
     end
     newArray
